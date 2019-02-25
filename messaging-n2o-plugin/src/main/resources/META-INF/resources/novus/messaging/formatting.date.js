a3043bb7e512ce129f010d723e704ffc372f1eac
Marat Khafizov <mhafizov@i-novus.ru>
2019-02-25 14:38:58 +0300

    Вынес конфиги в отдельный модуль

--- messaging-n2o-plugin/src/main/resources/META-INF/resources/novus/messaging/formatting.date.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-    var IN_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS';
-    return {
-        utcToLocal: function(value, format) {
-            return moment.utc(value, IN_FORMAT).tz(moment.tz.guess()).format(format);
-        }
-    };
-});
