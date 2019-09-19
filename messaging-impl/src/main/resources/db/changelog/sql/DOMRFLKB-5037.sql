8bbe0af06b187c1eab97e01a81e5fc0fa24eaddd
dbilalov <dbilalov@i-novus.ru>
2019-09-19 11:24:32 +0300

    cf DOMRFLKB-5037 Поправлены шаблоны LKB-PAYMENT-NTF-9,LKB-PAYMENT-NTF-3

--- messaging-impl/src/main/resources/db/changelog/sql/DOMRFLKB-5037.sql
+++ messaging-impl/src/main/resources/db/changelog/sql/DOMRFLKB-5037.sql
@@ -1,1 +1,2 @@
-update public.message_setting set text = replace(text, '%CONTRACT_NUMBER$', '%CONTRACT_NUMBER%');
+update public.message_setting set text = replace(text, '%CONTRACT_NUMBER$', '%CONTRACT_NUMBER%') where code='LKB-PAYMENT-NTF-9';
+update public.message_setting set text = replace(text, '%DEVELOPER_FULL_NAME$', '%DEVELOPER_FULL_NAME%') where code='LKB-PAYMENT-NTF-3';
