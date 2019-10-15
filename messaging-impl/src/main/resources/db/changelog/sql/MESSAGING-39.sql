2f794fe1a83c1617dc1b4c0763746e6dbfd7887a
agolodnaya <agolodnaya@i-novus.ru>
2019-10-15 12:44:55 +0300

    [cf] Уведомления. Пользователь. Пользовательские настройки уведомлений (часть 2) (MESSAGING-39)

--- messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-39.sql
+++ messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-39.sql
@@ -6,6 +6,6 @@ ALTER TABLE public.user_setting DROP CONSTRAINT IF EXISTS fk_user_setting_id;
 ALTER TABLE public.user_setting ADD CONSTRAINT user_setting_id_message_setting_id_fk FOREIGN KEY (id) REFERENCES message_setting(id);
 CREATE UNIQUE INDEX user_setting_user_id_msg_setting_id_ux ON public.user_setting USING btree(user_id, msg_setting_id);
 
-CREATE SEQUENCE user_setting_id_seq OWNED BY user_setting.id;
+CREATE SEQUENCE user_setting_id_seq;
 ALTER TABLE public.user_setting ALTER COLUMN id SET DEFAULT nextval('user_setting_id_seq');
 SELECT setval('user_setting_id_seq', (SELECT MAX(id) FROM public.user_setting));
