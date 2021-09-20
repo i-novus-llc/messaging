eacf239a31bc0b378fe4cce5b51a09351aaaac43
aarifullin <aarifullin@i-novus.ru>
2021-09-20 17:26:02 +0300

    [NF][MESSAGING-139] Рефакторинг схемы данных

---
+++ messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-139.sql
@@ -0,0 +1,24 @@
+CREATE TABLE messaging.system
+(
+    code VARCHAR PRIMARY KEY NOT NULL,
+    name VARCHAR
+);
+
+COMMENT ON TABLE messaging.system IS 'Системы';
+COMMENT ON COLUMN messaging.system.code IS 'Уникальный код системы';
+COMMENT ON COLUMN messaging.system.name IS 'Наименование системы';
+
+ALTER TABLE messaging.message
+    DROP COLUMN system_id;
+
+ALTER TABLE messaging.message
+    ADD COLUMN system_code VARCHAR REFERENCES messaging.system (code);
+COMMENT ON COLUMN messaging.message.system_code IS 'Система, к которой относится уведомление';
+
+ALTER TABLE messaging.message_setting
+    ADD COLUMN system_code VARCHAR REFERENCES messaging.system (code);
+COMMENT ON COLUMN messaging.message_setting.system_code IS 'Система, к которой относится настройка';
+
+ALTER TABLE messaging.user_setting
+    ADD COLUMN system_code VARCHAR REFERENCES messaging.system (code);
+COMMENT ON COLUMN messaging.user_setting.system_code IS 'Система, к которой относится пользовательская настройка';
