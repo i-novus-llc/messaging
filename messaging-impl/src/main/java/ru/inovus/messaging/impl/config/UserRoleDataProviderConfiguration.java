be2a65e33578b2721e487816fc718ae14ce24cfe
ilebedenko <ilebedenko@i-novus.ru>
2021-09-29 01:03:29 +0300

    [CF][MESSAGING-215] - some fix

--- messaging-impl/src/main/java/ru/inovus/messaging/impl/config/UserRoleDataProviderConfiguration.java
+++ messaging-impl/src/main/java/ru/inovus/messaging/impl/config/UserRoleDataProviderConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ResourceLoader;
 import ru.inovus.messaging.impl.UserRoleProvider;
 import ru.inovus.messaging.impl.provider.ConfigurableUserRoleProvider;
