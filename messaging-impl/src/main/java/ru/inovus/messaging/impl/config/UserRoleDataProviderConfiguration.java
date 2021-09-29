4f351c4a395ff38f83d7d2a4cb7de3b8a2190082
iryabov <iryabov@i-novus.ru>
2021-09-29 12:39:26 +0300

    MESSAGING-201 Removed unused imports

--- messaging-impl/src/main/java/ru/inovus/messaging/impl/config/UserRoleDataProviderConfiguration.java
+++ messaging-impl/src/main/java/ru/inovus/messaging/impl/config/UserRoleDataProviderConfiguration.java
@@ -1,13 +1,8 @@
 package ru.inovus.messaging.impl.config;
 
-import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
-import net.n2oapp.security.admin.rest.api.RoleRestService;
-import net.n2oapp.security.admin.rest.api.UserRestService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
