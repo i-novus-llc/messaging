553e3ae9392886fd309daddd11e4a630f8c13385
ilebedenko <ilebedenko@i-novus.ru>
2021-10-13 16:52:02 +0300

    [CF][MESSAGING-180] - remove component from xml

--- messaging-channel-web/src/test/java/ru/inovus/messaging/channel/web/TestApp.java
+++ messaging-channel-web/src/test/java/ru/inovus/messaging/channel/web/TestApp.java
@@ -5,17 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.test.context.ContextConfiguration;
 import ru.inovus.messaging.channel.web.config.EmbeddedKafkaTestConfiguration;
 import ru.inovus.messaging.channel.web.config.WebSecurityTestConfiguration;
 import ru.inovus.messaging.channel.web.configuration.EnableWebChannel;
-import ru.inovus.messaging.channel.web.configuration.WebSocketConfiguration;
-import ru.inovus.messaging.mq.support.kafka.KafkaConfig;
-import ru.inovus.messaging.mq.support.kafka.KafkaMqProvider;
 
 @SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
