9ffbaeb474235093a95ea3d465d679b15f29eb4d
aarifullin <aarifullin@i-novus.ru>
2021-11-01 19:05:18 +0300

    [NF][MESSAGING-231] добавил скрытие полей для шаблонов

--- messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-231.sql
+++ messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-231.sql
@@ -2,3 +2,8 @@ ALTER TABLE messaging.message
     ALTER COLUMN severity DROP NOT NULL;
 ALTER TABLE messaging.message
     ALTER COLUMN alert_type DROP NOT NULL;
+
+ALTER TABLE messaging.message_template
+    ALTER COLUMN severity DROP NOT NULL;
+ALTER TABLE messaging.message_template
+    ALTER COLUMN alert_type DROP NOT NULL;
