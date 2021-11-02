20510d46743c3797207e21a4fa4dffcf4b7082fc
aarifullin <aarifullin@i-novus.ru>
2021-11-02 11:19:10 +0300

    [NF][MESSAGING-231] review

--- messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-231.sql
+++ messaging-impl/src/main/resources/db/changelog/sql/MESSAGING-231.sql
@@ -1,6 +1,6 @@
-ALTER TABLE messaging.message
+ALTER TABLE messaging.message_template
     ALTER COLUMN severity DROP NOT NULL;
-ALTER TABLE messaging.message
+ALTER TABLE messaging.message_template
     ALTER COLUMN alert_type DROP NOT NULL;
 
 ALTER TABLE messaging.message_template
