1030963d37a4b0e4dd1e8f0bc20a82e8fec613e7
anigmatullin <anigmatullin@i-novus.ru>
2022-10-05 15:15:07 +0300

    [CF][EGISZNRPLATFORM-870] - Модификация модуля уведомлений

--- messaging-impl/src/test/java/ru/inovus/messaging/impl/provider/SecurityRecipientProviderTest.java
+++ messaging-impl/src/test/java/ru/inovus/messaging/impl/provider/SecurityRecipientProviderTest.java
@@ -1,11 +1,16 @@
 package ru.inovus.messaging.impl.provider;
 
+import net.n2oapp.security.admin.api.model.Organization;
+import net.n2oapp.security.admin.api.model.Region;
 import net.n2oapp.security.admin.api.model.Role;
 import net.n2oapp.security.admin.api.model.User;
 import net.n2oapp.security.admin.rest.api.OrganizationRestService;
 import net.n2oapp.security.admin.rest.api.RegionRestService;
 import net.n2oapp.security.admin.rest.api.RoleRestService;
 import net.n2oapp.security.admin.rest.api.UserRestService;
+import net.n2oapp.security.admin.rest.api.criteria.RestOrganizationCriteria;
+import net.n2oapp.security.admin.rest.api.criteria.RestRegionCriteria;
+import net.n2oapp.security.admin.rest.api.criteria.RestRoleCriteria;
 import net.n2oapp.security.admin.rest.api.criteria.RestUserCriteria;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +21,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.inovus.messaging.api.criteria.ProviderRecipientCriteria;
+import ru.inovus.messaging.api.criteria.SecurityBaseCriteria;
 import ru.inovus.messaging.api.model.ProviderRecipient;
 
 import java.util.List;
@@ -34,7 +40,6 @@ public class SecurityRecipientProviderTest {
     private RegionRestService regionRestService;
     @Mock
     private OrganizationRestService organizationRestService;
-
 
     @Before
     public void before() {
@@ -60,8 +65,32 @@ public class SecurityRecipientProviderTest {
         role1.setDescription("description1");
         user2.setRoles(List.of(role1));
 
+        Role role2 = new Role();
+        role2.setId(2);
+        role2.setName("name2");
+        role2.setCode("code2");
+        role2.setDescription("description2");
+
+        Region region = new Region();
+        region.setId(1);
+        region.setOkato("okato");
+        region.setCode("code");
+        region.setName("region");
+
+        Organization organization = new Organization();
+        organization.setId(1);
+        organization.setCode("code");
+        organization.setShortName("organization");
+        organization.setFullName("organization");
+
         PageImpl userPage = new PageImpl(List.of(user1, user2), new ProviderRecipientCriteria(), 2);
         Mockito.when(userRestService.findAll(new RestUserCriteria())).thenReturn(userPage);
+        PageImpl rolePage = new PageImpl(List.of(role1, role2), new SecurityBaseCriteria(), 2);
+        Mockito.when(roleRestService.findAll(new RestRoleCriteria())).thenReturn(rolePage);
+        PageImpl regionPage = new PageImpl(List.of(region), new SecurityBaseCriteria(), 2);
+        Mockito.when(regionRestService.getAll(new RestRegionCriteria())).thenReturn(regionPage);
+        PageImpl organizationPage = new PageImpl(List.of(organization), new SecurityBaseCriteria(), 2);
+        Mockito.when(organizationRestService.getAll(new RestOrganizationCriteria())).thenReturn(organizationPage);
 
         recipientProvider = new SecurityAdminRecipientProvider(userRestService, roleRestService, regionRestService, organizationRestService);
         MockitoAnnotations.initMocks(this);
@@ -84,5 +113,33 @@ public class SecurityRecipientProviderTest {
         assertEquals("surname2", content.get(1).getSurname());
         assertEquals("name2", content.get(1).getName());
         assertEquals("patronymic2", content.get(1).getPatronymic());
+    }
+
+    @Test
+    public void testGetRoles() {
+        List<Role> content = recipientProvider.getRoles(new SecurityBaseCriteria()).getContent();
+        assertEquals(2, content.size());
+        assertEquals("name1", content.get(0).getName());
+        assertEquals("code1", content.get(0).getCode());
+        assertEquals("name2", content.get(1).getName());
+        assertEquals("code2", content.get(1).getCode());
+    }
+
+    @Test
+    public void testGetRegions() {
+        List<Region> content = recipientProvider.getRegions(new SecurityBaseCriteria()).getContent();
+        assertEquals(1, content.size());
+        assertEquals("okato", content.get(0).getOkato());
+        assertEquals("code", content.get(0).getCode());
+        assertEquals("region", content.get(0).getName());
+    }
+
+    @Test
+    public void testGetMedOrganizations() {
+        List<Organization> content = recipientProvider.getMedOrganizations(new SecurityBaseCriteria()).getContent();
+        assertEquals(1, content.size());
+        assertEquals("code", content.get(0).getCode());
+        assertEquals("organization", content.get(0).getFullName());
+        assertEquals("organization", content.get(0).getShortName());
     }
 }
