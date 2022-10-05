80740d6dff6bed490dcee05421768e3c80484724
anigmatullin <anigmatullin@i-novus.ru>
2022-10-05 14:49:17 +0300

    [CF][EGISZNRPLATFORM-870] - Модификация модуля уведомлений

--- messaging-impl/src/test/java/ru/inovus/messaging/impl/service/AttachmentServiceTest.java
+++ messaging-impl/src/test/java/ru/inovus/messaging/impl/service/AttachmentServiceTest.java
@@ -1,9 +1,15 @@
 package ru.inovus.messaging.impl.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.sun.istack.ByteArrayDataSource;
 import net.n2oapp.platform.i18n.UserException;
 import net.n2oapp.platform.test.autoconfigure.EnableEmbeddedPg;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.impl.MetadataMap;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,18 +22,25 @@ import ru.inovus.messaging.api.criteria.RecipientCriteria;
 import ru.inovus.messaging.api.model.AttachmentResponse;
 import ru.inovus.messaging.impl.util.DocumentUtils;
 
+import javax.activation.DataHandler;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestApp.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = "novus.messaging.attachment.enabled: true")
 @TestPropertySource("classpath:application.properties")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @EnableEmbeddedPg
 public class AttachmentServiceTest {
 
@@ -44,11 +57,14 @@ public class AttachmentServiceTest {
     @Autowired
     private DocumentUtils documentUtils;
 
-    @Test
-    void createTest() {
+    @BeforeAll
+    void initialize() {
         service.create(List.of(getNewAttachment(FIRST_FILE_NAME), getNewAttachment(SECOND_FILE_NAME)), FIRST_MESSAGE_ID);
         service.create(List.of(getNewAttachment(THIRD_FILE_NAME)), SECOND_MESSAGE_ID);
+    }
 
+    @Test
+    void createTest() {
         RecipientCriteria criteria = new RecipientCriteria();
         Page<AttachmentResponse> responses = service.findAll(criteria);
         assertThat(responses.getContent().size(), is(3));
@@ -68,8 +84,31 @@ public class AttachmentServiceTest {
     void fileCountTest() {
         assertThrows(UserException.class, () -> service.create(List.of(getNewAttachment(FIRST_FILE_NAME),
                         getNewAttachment(SECOND_FILE_NAME),
-                        getNewAttachment(SECOND_FILE_NAME)),
+                        getNewAttachment(THIRD_FILE_NAME)),
                 THIRD_MESSAGE_ID));
+    }
+
+    @Test
+    void uploadTest() {
+        when(s3client.doesBucketExistV2(any())).thenReturn(true);
+        when(s3client.putObject(anyString(), anyString(), any(), any())).thenReturn(null);
+        AttachmentResponse response = service.upload(createAttachment());
+        assertThat(response.getShortFileName(), is(FIRST_FILE_NAME));
+    }
+
+    @Test
+    void downloadTest() {
+        when(s3client.getObject(anyString(), anyString())).thenReturn(new S3Object());
+        List<AttachmentResponse> responsesList = service.findAll(SECOND_MESSAGE_ID);
+        Response response = service.download(responsesList.get(0).getId());
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+        assertTrue(((String)response.getHeaders().get("Content-Disposition").get(0)).contains(THIRD_FILE_NAME));
+    }
+
+    @Test
+    void deleteTest() {
+        Response response = service.delete(FIRST_FILE_NAME);
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
     }
 
     private AttachmentResponse getNewAttachment(String fileName) {
@@ -78,5 +117,13 @@ public class AttachmentServiceTest {
         attachmentResponse.setFileName(documentUtils.getFileNameWithDateTime(fileName));
         attachmentResponse.setShortFileName(fileName);
         return attachmentResponse;
+    }
+
+    private Attachment createAttachment() {
+        MetadataMap<String, String> map = new MetadataMap();
+        map.add("Content-Disposition", String.format("filename=%s", FIRST_FILE_NAME));
+        return new Attachment("id",
+                new DataHandler(new ByteArrayDataSource(new byte[]{1}, "application/octet-stream")),
+                map);
     }
 }
