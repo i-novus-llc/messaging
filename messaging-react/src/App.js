2804ca1d254bb8dd5ff71862b2ea02cb76110094
aramazanov <aramazanov@i-novus.ru>
2023-04-16 20:34:48 +0300

    Миграция на н2о 23 версии

--- messaging-react/src/App.js
+++ messaging-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import N2O from 'n2o-framework';
-import {authProvider} from 'n2o-auth';
+import authProvider from 'n2o-framework/lib/core/auth/authProvider';
 import createFactoryConfig from 'n2o-framework/lib/core/factory/createFactoryConfig';
 
 const config = {
