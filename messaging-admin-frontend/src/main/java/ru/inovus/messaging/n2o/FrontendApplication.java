2a088d97ab8aa3e62071d9b12045dfb04e3ef7d5
Artur Nigmatullin <slave_to_money@mail.ruuser.email>
2023-04-24 11:18:54 +0300

    [CF][MESSAGING-244] - Переход на n2o 7.23 и платформу 5.1.1. Security

--- messaging-admin-frontend/src/main/java/ru/inovus/messaging/n2o/FrontendApplication.java
+++ messaging-admin-frontend/src/main/java/ru/inovus/messaging/n2o/FrontendApplication.java
@@ -1,19 +1,14 @@
 package ru.inovus.messaging.n2o;
 
-import net.n2oapp.security.admin.api.service.UserDetailsService;
 import net.n2oapp.security.admin.rest.client.AdminRestClientConfiguration;
 import net.n2oapp.security.auth.common.User;
-import net.n2oapp.security.auth.common.UserAttributeKeys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import ru.i_novus.ms.audit.client.UserAccessor;
-
-import java.util.Set;
 
 @SpringBootApplication
 @Import(AdminRestClientConfiguration.class)
@@ -21,15 +16,6 @@ public class FrontendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FrontendApplication.class, args);
-    }
-
-    @Bean
-    public UserService keycloakUserService(UserDetailsService userDetailsService, UserAttributeKeys userAttributeKeys) {
-        UserService userService = new UserService(userAttributeKeys, userDetailsService, "keycloak");
-        OidcUserService oidcUserService = new OidcUserService();
-        oidcUserService.setAccessibleScopes(Set.of("openid"));
-        userService.setDelegateOidcUserService(oidcUserService);
-        return userService;
     }
 
     @Bean
--- messaging-admin-frontend/src/main/java/ru/inovus/messaging/n2o/SecurityConfig.java
+++ messaging-admin-frontend/src/main/java/ru/inovus/messaging/n2o/SecurityConfig.java
@@ -1,9 +1,6 @@
 package ru.inovus.messaging.n2o;
 
-import net.n2oapp.security.admin.rest.client.AccountServiceRestClient;
 import net.n2oapp.security.auth.OpenIdSecurityCustomizer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,15 +11,6 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @Configuration
 public class SecurityConfig extends OpenIdSecurityCustomizer {
 
-    @Value("${access.service.userinfo-url}")
-    private String userInfoUri;
-
-    @Autowired
-    private AccountServiceRestClient accountServiceRestClient;
-
-    @Autowired
-    private UserService userService;
-
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
@@ -31,10 +19,8 @@ public class SecurityConfig extends OpenIdSecurityCustomizer {
     @Override
     protected void configureHttpSecurity(HttpSecurity http) throws Exception {
         super.configureHttpSecurity(http);
-        http.authorizeRequests().anyRequest().authenticated().and().oauth2Login().userInfoEndpoint(userInfo -> userInfo.oidcUserService(userService));
-        //todo 7.x.x security backend
-//        ContextUserInfoTokenServices tokenServices = new ContextUserInfoTokenServices(userInfoUri);
-//        http.addFilterAfter(new ContextFilter(tokenServices, accountServiceRestClient), FilterSecurityInterceptor.class);
+        http.authorizeRequests().anyRequest().authenticated().and().oauth2Login();
+        //todo 7.x.x security backend server
     }
 }
 
