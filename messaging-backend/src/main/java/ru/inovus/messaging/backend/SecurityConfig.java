7811582b9083cfdffcdca7fc3df5f88b82c86423
aarifullin <aarifullin@i-novus.ru>
2023-06-16 19:48:17 +0300

    [MESSAGING-248] redis session

--- messaging-backend/src/main/java/ru/inovus/messaging/backend/SecurityConfig.java
+++ messaging-backend/src/main/java/ru/inovus/messaging/backend/SecurityConfig.java
@@ -1,11 +1,10 @@
 package ru.inovus.messaging.backend;
 
-import net.n2oapp.platform.security.autoconfigure.N2oPlatformResourceServerConfigurerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,18 +12,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
-@EnableResourceServer
-public class SecurityConfig extends N2oPlatformResourceServerConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/info", "/api/api-docs", "/api/swagger**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf().disable();
+        return http.build();
     }
 
     @Bean
