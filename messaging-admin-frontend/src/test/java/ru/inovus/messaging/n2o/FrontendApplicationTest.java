198499109dbefc99b75921e83d7b68ca12bfb4ea
anigmatullin <anigmatullin@i-novus.ru>
2023-08-02 10:37:42 +0300

    [CF][EGISZNRPLATFORM-1436] - Перевести платформу 1.5 на lts версии java 11

--- messaging-admin-frontend/src/test/java/ru/inovus/messaging/n2o/FrontendApplicationTest.java
+++ messaging-admin-frontend/src/test/java/ru/inovus/messaging/n2o/FrontendApplicationTest.java
@@ -7,8 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = FrontendApplication.class,
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {"spring.config.import=optional:consul:"})
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FrontendApplicationTest {
     @Test
     public void contextLoads() {
