b344bfa3c4c12c5b756ecbc39913c70c708effc3
Каримуллин Ильяс <ikarimullin@i-novus.ru>
2024-11-29 18:51:58 +0300

    Add property to customize request matchers in SecurityConfig

--- messaging-backend/src/main/java/ru/inovus/messaging/backend/SecurityConfig.java
+++ messaging-backend/src/main/java/ru/inovus/messaging/backend/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.inovus.messaging.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +9,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
+
+    @Value("${novus.messaging.permit.all.matchers:#{T(java.util.List).of('/api/info', '/api-docs', '/api/swagger**', '/api/openapi**', '/api/index**')}}")
+    private List<String> permitAllMatchers;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest
                         .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-                        .requestMatchers("/api/info/", "/api/api-docs", "/api/swagger**", "/api/openapi**","/api/index**").permitAll()
+                        .requestMatchers(permitAllMatchers.toArray(new String[0])).permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable);
